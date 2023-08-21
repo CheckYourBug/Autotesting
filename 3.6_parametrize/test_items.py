0f68572a5a2f0ef276a1801faeffa45dd8fc3126
Vladislav Sholokhov <holohov.vladsilav@yandex.ru>
2023-08-21 22:19:14 +0500

    Add browser localization test

--- 3.6_parametrize/test_items.py
+++ 3.6_parametrize/test_items.py
@@ -6,5 +6,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_user_lang(browser):
     browser.get(link)
-    time.sleep(1)
+    time.sleep(30)
     assert browser.find_elements(By.CSS_SELECTOR, "input+[type='submit']"), 'basket button not found'
